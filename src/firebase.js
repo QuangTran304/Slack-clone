55edf555e4ba32c1f9a9e8f119efbc54aa844e0e
Quang Tran <nhatquang304@gmail.com>
2021-12-20 15:47:59 -0500

    added Header & firebase component

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyDOlC57ccDf_JPmfi5W_3hj3jllTy7bcxs',
+//   authDomain: 'slack-clone-a5b2a.firebaseapp.com',
+//   projectId: 'slack-clone-a5b2a',
+//   storageBucket: 'slack-clone-a5b2a.appspot.com',
+//   messagingSenderId: '340064235602',
+//   appId: '1:340064235602:web:1a5badfc8aa3babe64af8b',
+// };
