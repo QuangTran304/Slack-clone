9710909f76c1f63ca1c1891e11614fcb51711caf
Quang Tran <nhatquang304@gmail.com>
2021-12-22 16:34:11 -0500

    add SidebarOption component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,17 @@
 import styled from 'styled-components';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import CreateIcon from '@mui/icons-material/Create';
+import InsertCommentIcon from '@mui/icons-material/InsertComment';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import AppsIcon from '@mui/icons-material/Apps';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
+import SidebarOption from './SidebarOption';
 
 const Sidebar = () => {
   return (
@@ -13,8 +24,26 @@ const Sidebar = () => {
             Testing string
           </h3>
         </SidebarInfo>
+
         <CreateIcon />
       </SidebarHeader>
+
+      <SidebarOption Icon={InsertCommentIcon} title='Threads' />
+      <SidebarOption Icon={InboxIcon} title='Mentions & reactions' />
+      <SidebarOption Icon={DraftsIcon} title='Saved items' />
+      <SidebarOption Icon={BookmarkBorderIcon} title='Channel browser' />
+      <SidebarOption Icon={PeopleAltIcon} title='People & user groups' />
+      <SidebarOption Icon={AppsIcon} title='Apps' />
+      <SidebarOption Icon={FileCopyIcon} title='File browser' />
+      <SidebarOption Icon={ExpandLessIcon} title='Show less' />
+
+      <hr />
+
+      <SidebarOption Icon={ExpandMoreIcon} title='Channels' />
+
+      <hr />
+
+      <SidebarOption Icon={AddIcon} addChannelOption title='Add Channel' />
     </SidebarContainer>
   );
 };
@@ -28,6 +57,12 @@ const SidebarContainer = styled.div`
   border-top: 1px solid #49274b;
   max-width: 260px;
   margin-top: 60px;
+
+  > hr {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #49274b;
+  }
 `;
 
 const SidebarHeader = styled.div`
